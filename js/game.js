218a4e862ba3565bf02392a76e00e46fd72d8cad
Oscar <oscarlouw@gmail.com>
2020-04-09 18:49:06 +0200

    Lag fixed, animation basics for movemenT

--- js/game.js
+++ js/game.js
@@ -1,12 +1,22 @@
 // Game configuration
 var config = {
-    type: Phaser.AUTO,
+    type: Phaser.CANVAS,
     backgroundColor: '#000000',
     scale: {
         mode: Phaser.Scale.RESIZE,
         parent: 'phaser-game',
         width: '100%',
         height: '100%'
+    },
+    physics: {
+        default: 'matter',
+        matter: {
+            debug: {
+                renderFill: false,
+                showInternalEdges: true,
+                showConvexHulls: true
+            }
+        }
     },
     scene: [BootGame, Moor] // list of scenes in the project
 }
--- js/scenes/Moor.js
+++ js/scenes/Moor.js
@@ -13,9 +13,10 @@ class Moor extends Phaser.Scene {
     // Loads all assets before scene starts
     preload(){
         this.load.image("background", "../../img/tilingGrass.png");
-        this.load.spritesheet("player", "../../img/player/player_spritesheet.png", {
-            frameWidth: 512,
-            frameHeight: 512
+        this.load.image("car", "../../img/placeholderCar.png");
+        this.load.spritesheet("player", "../../img/player/playerSprites.png", {
+            frameWidth: 128,
+            frameHeight: 128
         });
     }
 
@@ -30,15 +31,19 @@ class Moor extends Phaser.Scene {
         this.background = this.add.tileSprite(0,0, this.sceneConfig.sceneWidth, this.sceneConfig.sceneHeight, "background");
         this.background.setOrigin(0,0);
 
+        this.car = this.add.image(500,500, "car");
+        this.car.setOrigin(0.5,0.5);
+        this.car.setScale(0.8, 0.8);
+
         // Sprites
-        this.player = this.add.sprite(500, 500, "player");
+        this.player = this.add.sprite(128, 128, "player");
         this.player.setOrigin(0.5,1);
-        this.player.setScale(.3,.3);
+        //this.player.setScale(.3,.3);
         this.player.targetPosition = new Phaser.Math.Vector2(500, 500);
     }
 
     update(time, delta){
-        this.movePlayer(0.6 * delta);
+        this.movePlayer(0.3 * delta);
     }
 
     movePlayer(moveSpeed){
@@ -51,6 +56,38 @@ class Moor extends Phaser.Scene {
             moveDirection.normalize();
             moveDirection.scale(moveSpeed);
         }
+
+        if(length < 0.2){
+            // idle
+            this.player.setFrame(1);
+        } else{
+            if(Math.abs(moveDirection.x) > Math.abs(moveDirection.y)){
+                if(moveDirection.x < 0){
+                    // right
+                    this.player.setFrame(3);
+                } else if(moveDirection.x > 0){
+                    // left
+                    this.player.setFrame(2);
+                }
+            } else {
+                if(moveDirection.y < 0){
+                    // down
+                    this.player.setFrame(1);
+                } else if(moveDirection.y > 0)
+                {
+                    // up
+                    this.player.setFrame(4);
+                }
+            }
+        }
+
+        
+        
+        
+
+        
+
+        
 
         var newPosition = currentPosition.subtract(moveDirection);
         this.player.setPosition(newPosition.x, newPosition.y);
