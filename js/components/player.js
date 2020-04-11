c3cf89ef58416649fe329c82a0bc7ef2d8325e71
Felipe Dacal Fragoso <fefragoso@gmail.com>
2020-04-11 17:21:25 +0200

    add player class with move function

--- js/components/player.js
+++ js/components/player.js
@@ -1,3 +1,6 @@
+const velocity = 2000;
+const updateBy = 2;
+
 class Player extends Phaser.Physics.Matter.Sprite {
     constructor(scene, shape) {
 
@@ -6,16 +9,55 @@ class Player extends Phaser.Physics.Matter.Sprite {
         var y = 0;
        
         //add player on the screen
-        super(scene.matter.world, x, y, "car");
+        super(scene.matter.world, x, y, "player");
         scene.add.existing(this);
         
         //set player properties
         this.setBody(shape);
-        this.setScale(0.4, 0.4);
         this.setPosition(200, 200);
         this.setOrigin(0.5, 1);
         this.setDepth(1);
-        this.setStatic(true); 
         this.targetPosition = new Phaser.Math.Vector2(200, 200);
+    }
+    
+    movePlayer(moveSpeed) {
+        var currentPosition = new Phaser.Math.Vector2(this.x, this.y);
+        var moveDirection = new Phaser.Math.Vector2(currentPosition.x, currentPosition.y);
+
+        moveDirection.subtract(this.targetPosition);
+        var length = moveDirection.length();
+
+        if (length > moveSpeed) {
+            moveDirection.normalize();
+            moveDirection.scale(moveSpeed);
+        }
+
+        if (length < 0.2) {
+            // idle
+            this.setFrame(1);
+        } else {
+            if (Math.abs(moveDirection.x) > Math.abs(moveDirection.y)) {
+                if (moveDirection.x < 0) {
+                    // right
+                    this.setFrame(3);
+                } else if (moveDirection.x > 0) {
+                    // left
+                    this.setFrame(2);
+                }
+            } else {
+                if (moveDirection.y < 0) {
+                    // down
+                    this.setFrame(1);
+                } else if (moveDirection.y > 0) {
+                    // up
+                    this.setFrame(0);
+                }
+            }
+        }
+
+        console.log(-moveDirection.x + " ### " + -moveDirection.y);
+        //var newPosition = currentPosition.subtract(moveDirection);
+        this.setVelocity(-moveDirection.x, -moveDirection.y);
+        this.setAngle(0);
     }
 }
--- js/scenes/Moor.js
+++ js/scenes/Moor.js
@@ -14,13 +14,13 @@ class Moor extends Phaser.Scene {
         this.load.image("background", "../../img/tilingGrass.png");
         this.load.image("car", "../../img/car.png");
         this.load.spritesheet("player", "../../img/player/playerSprites.png", {
-            frameWidth: 128, //128
-            frameHeight: 128 //128
+            frameWidth: 128, 
+            frameHeight: 128 
         });
 
         this.load.spritesheet("npc01", "../../img/player/playerSprites.png", {
-            frameWidth: 128, //128
-            frameHeight: 128 //128
+            frameWidth: 128, 
+            frameHeight: 128 
         });
 
         // Load body shapes from JSON file generated using PhysicsEditor
@@ -46,45 +46,27 @@ class Moor extends Phaser.Scene {
 
         this.car = new Car(this, shapes.carBody);
 
-        // Sprites
-        this.player = this.matter.add.sprite(0, 0, "player");
-        //this.player.sprite.yOffset = 1;
-        this.player.setBody(shapes.player);
-        this.player.setOrigin(0.5, 1);
-        this.player.setPosition(200, 200);
-        //this.player.body.inertia = Infinity;
-        //this.player.setScale(.3,.3);
-        this.player.targetPosition = new Phaser.Math.Vector2(200, 200);
-        this.player.setDepth(1);
+        this.player = new Player(this, shapes.player);
 
-        this.npc01 = this.matter.add.sprite(0, 0, "npc01");
+        /*this.npc01 = this.matter.add.sprite(0, 0, "npc01");
         this.npc01.setBody(shapes.player);
         this.npc01.setOrigin(0.5, 1);
         this.npc01.setPosition(1200, 300);
         this.npc01.targetPosition = new Phaser.Math.Vector2(200, 200);
         this.npc01.setDepth(1);
-        this.npc01.setStatic(true);
+        this.npc01.setStatic(true);*/
 
         // Camera smooth following
         this.cameras.main.startFollow(this.player, false, 0.05, 0.05);
 
         // Physics
         this.matter.world.setBounds(0, 0, this.sceneConfig.sceneWidth, this.sceneConfig.sceneHeight);
-
-        /*  this.matter.world.on('collisionstart', function (event, player, car) {
-
-            player.gameObject.setTint(0xff0000);
-            car.gameObject.setTint(0x00ff00);
-    
-        }); */
-
-        
-
-
     }
 
     update(time, delta) {
-        this.movePlayer(.3 * delta);
+        //this.movePlayer(.3 * delta);
+
+        this.player.movePlayer(.3 * delta);
 
         //Change the layer depth of car and player
         if (this.player.y > this.car.y) {
@@ -94,45 +76,6 @@ class Moor extends Phaser.Scene {
             this.player.setDepth(1);
             this.car.setDepth(2);
         }
-    }
-
-    movePlayer(moveSpeed) {
-        var currentPosition = new Phaser.Math.Vector2(this.player.x, this.player.y);
-        var moveDirection = new Phaser.Math.Vector2(currentPosition.x, currentPosition.y);
-        moveDirection.subtract(this.player.targetPosition);
-        var length = moveDirection.length();
-
-        if (length > moveSpeed) {
-            moveDirection.normalize();
-            moveDirection.scale(moveSpeed);
-        }
-
-        if (length < 0.2) {
-            // idle
-            this.player.setFrame(1);
-        } else {
-            if (Math.abs(moveDirection.x) > Math.abs(moveDirection.y)) {
-                if (moveDirection.x < 0) {
-                    // right
-                    this.player.setFrame(3);
-                } else if (moveDirection.x > 0) {
-                    // left
-                    this.player.setFrame(2);
-                }
-            } else {
-                if (moveDirection.y < 0) {
-                    // down
-                    this.player.setFrame(1);
-                } else if (moveDirection.y > 0) {
-                    // up
-                    this.player.setFrame(0);
-                }
-            }
-        }
-
-        //var newPosition = currentPosition.subtract(moveDirection);
-        this.player.setVelocity(-moveDirection.x, -moveDirection.y);
-        this.player.setAngle(0);
     }
 
     handleClick(pointer) {
