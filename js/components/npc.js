8d1dbcce6d25995ecb71da9cb8d388f332ff45b8
Felipe Dacal Fragoso <fefragoso@gmail.com>
2020-04-11 18:13:54 +0200

    Update npc.js

--- js/components/npc.js
+++ js/components/npc.js
@@ -2,8 +2,8 @@ class NPC extends Phaser.Physics.Matter.Sprite {
     constructor(scene, npcName, shape) {
 
         //npc position on the screen
-        var x = 0;
-        var y = 0;
+        var x = 1800;
+        var y = 290;
        
         //add npc on the screen
         super(scene.matter.world, x, y, npcName);
@@ -11,9 +11,11 @@ class NPC extends Phaser.Physics.Matter.Sprite {
         
         //set npc properties
         this.setBody(shape);
-        this.setPosition(200, 200);
+        this.setScale(0.4, 0.4);
+        this.setPosition(x, y);
         this.setOrigin(0.5, 1);
         this.setDepth(1);
+        this.setStatic(true);
         this.targetPosition = new Phaser.Math.Vector2(200, 200);
     }
 }
