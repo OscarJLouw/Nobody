2542d09648b2355d1d263763c796ee7a7c15d373
Felipe Dacal Fragoso <fefragoso@gmail.com>
2020-04-21 18:57:16 +0200

    comic manager using json

--- js/components/ComicManager.js
+++ js/components/ComicManager.js
@@ -3,58 +3,33 @@ class ComicManager {
         this.scene = scene;
         this.visiblePannels = [];
         this.currentlyInComic = false;
-
-        // Page List
-        this.pageList = [
-            {
-                pageName: "Intro_1",
-                pannels: [
-                    {name: "testPanel1", animation: "slide", direction: "up"},
-                    {name: "testPanel2", animation: "fade"},
-                    {name: "testPanel3", animation: "fade"}
-                ]
-            },
-            {
-                pageName: "Intro_2",
-                pannels: [
-                    {name: "testPanel4", animation: "slide", direction: "left"},
-                    {name: "testPanel5", animation: "slide", direction: "right"}
-                ],
-                animations: [
-                    "none",
-                    "none"
-                ]
-            }
-        ];
-
-        // Comic List
-        this.comicList = [
-            {
-                comicName: "Introduction",
-                pages: [
-                    {name:"Intro_1", next:"Intro_2"},
-                    {name:"Intro_2", next:"none"}
-                ]
-            }
-        ]
     }
 
-    loadComics(scene){
+    loadComics(scene) {
         scene.load.image("testPanel1", "../../img/Comics/Intro/testpanel1.png");
         scene.load.image("testPanel2", "../../img/Comics/Intro/testpanel2.png");
         scene.load.image("testPanel3", "../../img/Comics/Intro/testpanel3.png");
         scene.load.image("testPanel4", "../../img/Comics/Intro/testpanel4.png");
         scene.load.image("testPanel5", "../../img/Comics/Intro/testpanel5.png");
+        scene.load.json('pageList', '../../json/intro.json');
+        scene.load.json('comicList', '../../json/comic.json');
     }
 
-    startComic(comicName){
+    loadJSONComics(scene) {
+        this.comicList = scene.cache.json.get('comicList').comicList;
+        this.pageList = scene.cache.json.get('pageList').pageList;
+    }
+
+    startComic(comicName) {
+
         this.currentlyInComic = true;
+
         this.currentComic = this.comicList.find(x => x.comicName === comicName);
         this.currentPageIndex = 0;
         this.currentPage = this.pageList.find(x => x.pageName === this.currentComic.pages[0].name);
         this.currentPannelIndex = 0;
-        
-        if(comicName != "Introduction"){
+
+        if (comicName != "Introduction") {
             this.scene.tweens.add({
                 targets: this.scene.overlay,
                 alpha: 0.8,
@@ -66,19 +41,19 @@ class ComicManager {
         this.drawPannel(this.currentPage.pannels[this.currentPannelIndex]);
     }
 
-    drawPannel(pannelObject){
+    drawPannel(pannelObject) {
         var pannel = this.scene.add.image(this.scene.cameras.main.centerX, this.scene.cameras.main.centerY, pannelObject.name);
 
         var aspectRatio = pannel.displayWidth / pannel.displayHeight;
         pannel.displayHeight = this.scene.cameras.main.displayHeight;
         pannel.displayWidth = pannel.displayHeight * aspectRatio;
-        
+
         pannel.setDepth(99999);
         pannel.setScrollFactor(0);
 
         this.visiblePannels.push(pannel);
 
-        if(pannelObject.animation == "fade"){
+        if (pannelObject.animation == "fade") {
             pannel.setAlpha(0);
             this.scene.tweens.add({
                 targets: pannel,
@@ -86,8 +61,8 @@ class ComicManager {
                 duration: 1000,
                 ease: 'Power2'
             });
-        } else if(pannelObject.animation == "slide"){
-            switch(pannelObject.direction){
+        } else if (pannelObject.animation == "slide") {
+            switch (pannelObject.direction) {
                 case "up":
                     pannel.y = this.scene.cameras.main.displayHeight * 2;
                     break;
@@ -114,15 +89,15 @@ class ComicManager {
 
     }
 
-    nextPannel(){
+    nextPannel() {
         this.currentPannelIndex++;
 
-        if(this.currentPage.pannels[this.currentPannelIndex] != null){
+        if (this.currentPage.pannels[this.currentPannelIndex] != null) {
             this.drawPannel(this.currentPage.pannels[this.currentPannelIndex]);
-            
+
             return false;
         } else {
-            for(var i = 0; i<this.visiblePannels.length; i++){
+            for (var i = 0; i < this.visiblePannels.length; i++) {
                 this.visiblePannels[i].destroy();
             }
 
@@ -132,13 +107,13 @@ class ComicManager {
         }
     }
 
-    nextPage(){
+    nextPage() {
 
-        if(this.currentComic.pages[this.currentPageIndex].next == "none"){
+        if (this.currentComic.pages[this.currentPageIndex].next == "none") {
             // Comic is finished
             this.currentlyInComic = false;
 
-            if(this.currentComic.comicName != "Introduction"){
+            if (this.currentComic.comicName != "Introduction") {
                 this.scene.tweens.add({
                     targets: this.scene.overlay,
                     alpha: 0,
